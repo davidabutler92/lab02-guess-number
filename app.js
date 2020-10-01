ab10ef6d72c9a05adbc7eef07dbc3d51f5003ab4
davidabutler92 <davidabutler92@yahoo.com>
2020-09-30 18:26:51 -0700

    add fix white space and delete console.log

--- app.js
+++ app.js
@@ -7,34 +7,39 @@ const button = document.getElementById('guess-button');
 const playAgainButton = document.getElementById('play-again');
 
 let guessesLeft = 3;
-const randomNumber = Math.ceil(Math.random() * 5);
-
+const winningNumber = Math.ceil(Math.random() * 5);
 
 button.addEventListener('click', () => {
-    console.log(randomNumber);
     const guess = Number(userGuess.value); 
-    const comparedResults = compareNumbers(guess, randomNumber);
-
-    if (comparedResults === 0) {
+    const comparedResults = compareNumbers(guess, winningNumber);
+    if (guessesLeft === 0) {
+        results.textContent = 'Oops, you ran out of guesses, you lose!';
+        numOfGuessLeft.textContent = 0;
+        button.style.display = 'none';
+        playAgainButton.style.display = 'block';
+    } else if (comparedResults === 0) {
         results.textContent = ('YAY! WINNER WINNER CHICKEN DINNER!');
         button.style.display = 'none';
         playAgainButton.style.display = 'block';
+        guessesLeft--;
+        numOfGuessLeft.textContent = guessesLeft;
     } else if (comparedResults === -1) {
         results.textContent = 'Oof, Your guess was too low, guess again.';
         guessesLeft--;
         numOfGuessLeft.textContent = guessesLeft;
     } else if (comparedResults === 1) {
-        results.textContent = 'Oof, your guess was too high, gues again.';
+        results.textContent = 'Oof, your guess was too high, guess again.';
         guessesLeft--;
         numOfGuessLeft.textContent = guessesLeft; 
-    } else if (guessesLeft === 0) {
-        results.textContent = 'Oops, you ran out of guesses, you lose!';
-        button.style.display = 'none';
-        playAgainButton.style.display = 'block';
-    } 
-
+    }
+    
     playAgainButton.addEventListener('click', () => {
-        
+        guessesLeft = 3;
+        numOfGuessLeft.textContent = guessesLeft;
+        userGuess.value = '';
+        results.textContent = '';
+        playAgainButton.style.display = 'none';
+        button.style.display = 'block';
+        //loaction.reload();  This does the same thing as content above ^ only downside is that you have to refresh the page.
     });
-
 });
